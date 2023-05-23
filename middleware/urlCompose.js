14f4be6e57fc8b1722db6b0d17dd318720c1f222
Matteo Giusti <suikofire@gmail.com>
2023-05-23 19:27:55 +0200

    Fixes explicit port breaking production links

--- middleware/urlCompose.js
+++ middleware/urlCompose.js
@@ -1,11 +1,11 @@
-import { BASE_URL, LISTEN_PORT } from "../config.js";
+import {BASE_URL, isDevelop, LISTEN_PORT} from "../config.js";
 
 const compose = ( path ) => {
 
     if(!path.startsWith('/'))
         path = `/${path}`;
         
-    if(LISTEN_PORT === 80 || LISTEN_PORT === 443) {
+    if(!isDevelop || LISTEN_PORT === 80 || LISTEN_PORT === 443) {
         return `${BASE_URL}${path}`;
     }
 
